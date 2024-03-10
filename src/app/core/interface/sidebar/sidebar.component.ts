987a5f8416aaadd8aa7ba8fa3ba3689a1d82c2ac
Helgeee <olegvotoz@gmail.com>
2024-03-10 22:33:53 +0300

    добавлены компоненты

---
+++ src/app/core/interface/sidebar/sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component( {
+    selector: 'my-sidebar' ,
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.scss']
+})
+
+export class MySideBar implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {
+        
+    }
+}
